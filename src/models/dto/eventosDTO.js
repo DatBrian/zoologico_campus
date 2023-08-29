3728166b7a36302f82c36cc59dc8c4cc7156f2d0
JoseCabrejoVillarCampus <cabre920903@gmail.com>
2023-08-29 16:31:59 -0500

    feat: :sparkles: Agregamos los campos faltantes a los DTO
    agregamos los nuevos campos insertados de los esquemas a los dtos

--- src/models/dto/eventosDTO.js
+++ src/models/dto/eventosDTO.js
@@ -21,6 +21,16 @@ export const CheckPostAllEvents= [
     .notEmpty().withMessage("El campo description_info es Obligatorio")
     .isString().withMessage("El campo description_info debe ser de tipo String")
     .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$/).withMessage("Solo admite letras"),
+
+    check("hora_inicio")
+    .notEmpty().withMessage("El campo start_time es Obligatorio")
+    .isString().withMessage("El campo start_time debe ser de tipo String")
+    .matches(/^(0?[1-9]|1[0-2]):[0-5]\\d\\s?(am|pm|AM|PM)$/).withMessage("Solo admite letras"),
+
+    check("hora_finalizacion")
+    .notEmpty().withMessage("El campo end_time es Obligatorio")
+    .isString().withMessage("El campo end_time debe ser de tipo String")
+    .matches(/^(0?[1-9]|1[0-2]):[0-5]\\d\\s?(am|pm|AM|PM)$/).withMessage("Solo admite letras"),
 ]
 
 
