90c99d7edc448af2bcf6841def1ea5bc48b36e21
DatBrian <vikard.2005@hotmail.com>
2023-08-29 19:36:16 -0500

    feat: :card_file_box: Se agregaron los esquemas y una automatización para los mismos
    Se agregaron todos los esquemas para el proyecto y se implementó una automatización para crear toda la base de datos con datos de prueba al levantar el servidor

---
+++ src/models/schemas/AnimalesSchema.js
@@ -0,0 +1,171 @@
+import ClientError from "../../utils/ClientError.js";
+
+class AnimalesSchema {
+    constructor(database) {
+        this.database = database;
+        this.entity = "animales";
+        this.collection = this.database.collection(this.entity);
+    }
+
+    async generateCollection() {
+        try {
+            await this.database.createCollection(this.entity, {
+                capped: true,
+                size: 10000,
+                max: 10,
+                validator: {
+                    $jsonSchema: {
+                        bsonType: "object",
+                        additionalProperties: false,
+                        required: [
+                            "nombre",
+                            "especie",
+                            "clase",
+                            "subclase",
+                            "pais_origen",
+                            "estado",
+                            "dato_curioso",
+                            "zona",
+                            "area",
+                        ],
+                        properties: {
+                            _id: {
+                                bsonType: "objectId",
+                            },
+                            nombre: {
+                                bsonType: "string",
+                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$",
+                                description:
+                                    "Debe informar el campo name y este debe ser un string",
+                            },
+                            especie: {
+                                bsonType: "string",
+                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$",
+                                description:
+                                    "Debe informar el campo species y este debe ser un string",
+                            },
+                            clase: {
+                                bsonType: "string",
+                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$",
+                                description:
+                                    "Debe informar el campo class y este debe ser un string",
+                            },
+                            subclase: {
+                                bsonType: "string",
+                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$",
+                                description:
+                                    "Debe informar el campo sub_class y este debe ser un string",
+                            },
+                            pais_origen: {
+                                bsonType: "string",
+                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$",
+                                description:
+                                    "Debe informar el campo origin y este debe ser un string",
+                            },
+                            estado: {
+                                bsonType: "string",
+                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$",
+                                description:
+                                    "Debe informar el campo state y este debe ser un string",
+                            },
+                            dato_curioso: {
+                                bsonType: "string",
+                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$",
+                                description:
+                                    "Debe informar el campo curiosity y este debe ser un string",
+                            },
+                            zona: {
+                                bsonType: "string",
+                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$",
+                                description:
+                                    "Debe informar el campo zone y este debe ser un string",
+                            },
+                            area: {
+                                bsonType: "string",
+                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$",
+                                description:
+                                    "Debe informar el campo belonging_area y este debe ser un string",
+                            },
+                        },
+                    },
+                },
+            });
+        } catch (error) {
+            new ClientError(`Error al generar la colección de ${this.entity}`);
+            throw error.message;
+        }
+    }
+
+    async createData() {
+        try {
+            await this.collection.insertMany([
+                {
+                    nombre: "León",
+                    especie: "Panthera leo",
+                    clase: "Mammalia",
+                    subclase: "Theria",
+                    pais_origen: "África",
+                    estado: "Vulnerable",
+                    dato_curioso:
+                        "Los leones son conocidos por ser los reyes de la selva.",
+                    zona: "Savana",
+                    area: "Reserva Natural",
+                },
+                {
+                    nombre: "Elefante africano",
+                    especie: "Loxodonta africana",
+                    clase: "Mammalia",
+                    subclase: "Theria",
+                    pais_origen: "África",
+                    estado: "Amenazado",
+                    dato_curioso:
+                        "Los elefantes son los animales terrestres más grandes del mundo.",
+                    zona: "Selva",
+                    area: "Parque Nacional",
+                },
+                {
+                    nombre: "Tigre de Bengala",
+                    especie: "Panthera tigris tigris",
+                    clase: "Mammalia",
+                    subclase: "Theria",
+                    pais_origen: "India",
+                    estado: "En peligro",
+                    dato_curioso:
+                        "Los tigres son cazadores solitarios y excelentes nadadores.",
+                    zona: "Bosque",
+                    area: "Reserva de Tigres",
+                },
+                {
+                    nombre: "Pingüino emperador",
+                    especie: "Aptenodytes forsteri",
+                    clase: "Aves",
+                    subclase: "Neornithes",
+                    pais_origen: "Antártida",
+                    estado: "Casi amenazado",
+                    dato_curioso:
+                        "Los pingüinos emperador son conocidos por su impresionante forma de crianza.",
+                    zona: "Antártida",
+                    area: "Colonia de Pingüinos",
+                },
+                {
+                    nombre: "Tortuga marina",
+                    especie: "Chelonia mydas",
+                    clase: "Reptilia",
+                    subclase: "Diapsida",
+                    pais_origen: "Océanos del mundo",
+                    estado: "En peligro crítico",
+                    dato_curioso:
+                        "Las tortugas marinas viajan miles de millas para anidar en las playas donde nacieron.",
+                    zona: "Océano",
+                    area: "Hábitat Marino",
+                },
+            ]);
+        } catch (error) {
+            new ClientError(
+                `Ocurrió un error al generar los datos de ${this.entity}`
+            );
+            throw error.message;
+        }
+    }
+}
+export default AnimalesSchema;
---
+++ src/models/schemas/EmpleadosSchema.js
@@ -0,0 +1,153 @@
+import ClientError from "../../utils/ClientError.js";
+
+class EmpleadosSchema {
+    constructor(database) {
+        this.database = database;
+        this.entity = "empleados";
+        this.collection = this.database.collection(this.entity);
+    }
+
+    async generateCollection() {
+        try {
+            await this.database.createCollection(this.entity, {
+                capped: true,
+                size: 10000,
+                max: 10,
+                validator: {
+                    $jsonSchema: {
+                        bsonType: "object",
+                        additionalProperties: false,
+                        required: [
+                            "nombre_completo",
+                            "cargo",
+                            "direccion",
+                            "email",
+                            "telefono",
+                            "area_asignada",
+                            "zona",
+                            "jornada",
+                        ],
+                        properties: {
+                            _id: {
+                                bsonType: "objectId",
+                            },
+                            nombre_completo: {
+                                bsonType: "string",
+                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$",
+                                description:
+                                    "Debe informar el campo complete_name y este debe ser un string",
+                            },
+                            cargo: {
+                                bsonType: "string",
+                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$",
+                                description:
+                                    "Debe informar el campo assigned_position y este debe ser un string",
+                            },
+                            direccion: {
+                                bsonType: "string",
+                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$",
+                                description:
+                                    "Debe informar el campo address y este debe ser un string",
+                            },
+                            email: {
+                                bsonType: "string",
+                                description:
+                                    "Debe informar un campo Email válido",
+                            },
+                            telefono: {
+                                bsonType: "string",
+                                description:
+                                    "Debe informar el campo phone_number y este debe ser un string",
+                            },
+                            area_asignada: {
+                                bsonType: "string",
+                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$",
+                                description:
+                                    "Debe informar el campo belonging_area y este debe ser un string",
+                            },
+                            zona: {
+                                bsonType: "string",
+                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$",
+                                description:
+                                    "Debe informar el campo zone y este debe ser un string",
+                            },
+                            jornada: {
+                                bsonType: "string",
+                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$",
+                                description:
+                                    "Debe informar el campo schedule y este debe ser un string",
+                            },
+                        },
+                    },
+                },
+            });
+        } catch (error) {
+            new ClientError(
+                `Ocurrió un error al generar la colección de ${this.entity}`
+            );
+            throw error.message;
+        }
+    }
+
+    async createData() {
+        try {
+            await this.collection.insertMany([
+                {
+                    nombre_completo: "Juan Perez",
+                    cargo: "Gerente",
+                    direccion: "Calle 123, Ciudad",
+                    email: "juan@example.com",
+                    telefono: "123-456-7890",
+                    area_asignada: "Ventas",
+                    zona: "Norte",
+                    jornada: "Mañana",
+                },
+                {
+                    nombre_completo: "Maria Lopez",
+                    cargo: "Analista",
+                    direccion: "Avenida 456, Otra Ciudad",
+                    email: "maria@example.com",
+                    telefono: "987-654-3210",
+                    area_asignada: "Desarrollo",
+                    zona: "Sur",
+                    jornada: "Tarde",
+                },
+                {
+                    nombre_completo: "Carlos Rodriguez",
+                    cargo: "Técnico",
+                    direccion: "Calle 789, Ciudad",
+                    email: "carlos@example.com",
+                    telefono: "555-555-5555",
+                    area_asignada: "Soporte",
+                    zona: "Este",
+                    jornada: "Noche",
+                },
+                {
+                    nombre_completo: "Laura Gomez",
+                    cargo: "Diseñadora",
+                    direccion: "Calle 456, Ciudad",
+                    email: "laura@example.com",
+                    telefono: "123-987-6543",
+                    area_asignada: "Diseño",
+                    zona: "Oeste",
+                    jornada: "Mañana",
+                },
+                {
+                    nombre_completo: "Pedro Ramirez",
+                    cargo: "Desarrollador",
+                    direccion: "Avenida 789, Ciudad",
+                    email: "pedro@example.com",
+                    telefono: "789-123-4560",
+                    area_asignada: "Desarrollo",
+                    zona: "Centro",
+                    jornada: "Tarde",
+                },
+            ]);
+        } catch (error) {
+            console.log(error);
+            new ClientError(`Error al generar la colección de ${this.entity}`);
+            throw error.message;
+        }
+    }
+}
+export default EmpleadosSchema;
