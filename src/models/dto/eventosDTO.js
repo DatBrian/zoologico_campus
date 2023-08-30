6268f69f01cdcd03de0c782db7ad977057756446
JoseCabrejoVillarCampus <cabre920903@gmail.com>
2023-08-29 20:48:07 -0500

    refactor: :recycle: Ajustes a los dtos, basados en los nuevos patrones de los esquemas

--- src/models/dto/eventosDTO.js
+++ src/models/dto/eventosDTO.js
@@ -5,22 +5,22 @@ export const CheckPostAllEvents= [
     check("fecha")
     .notEmpty().withMessage("El campo date es Obligatorio")
     .isString().withMessage("El campo date debe ser de tipo String")
-    .matches(/^(?:\d{4})-(?:0[1-9]|1[0-2])-(?:0[1-9]|[12]\d|3[01])$/).withMessage("Solo admite numeros en formato 0000-00-00 "),
+    .matches(/^(?:\\d{4})-(?:0[1-9]|1[0-2])-(?:0[1-9]|[12]\\d|3[01])$/).withMessage("Solo admite numeros en formato 0000-00-00 "),
 
     check("estado")
     .notEmpty().withMessage("El campo state es Obligatorio")
     .isString().withMessage("El campo state debe ser de tipo String")
-    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$/).withMessage("Solo admite letras"),
+    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$/).withMessage("Solo admite letras"),
 
     check("area")
     .notEmpty().withMessage("El campo belonging_area es Obligatorio")
     .isString().withMessage("El campo belonging_area debe ser de tipo String")
-    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$/).withMessage("Solo admite letras"),
+    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$/).withMessage("Solo admite letras"),
 
     check("descripcion")
     .notEmpty().withMessage("El campo description_info es Obligatorio")
     .isString().withMessage("El campo description_info debe ser de tipo String")
-    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$/).withMessage("Solo admite letras"),
+    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$/).withMessage("Solo admite letras"),
 
     check("hora_inicio")
     .notEmpty().withMessage("El campo start_time es Obligatorio")
--- src/models/dto/paseDTO.js
+++ src/models/dto/paseDTO.js
@@ -5,22 +5,22 @@ export const CheckPostAllPases= [
     check("nombre")
     .notEmpty().withMessage("El campo name es Obligatorio")
     .isString().withMessage("El campo name debe ser de tipo String")
-    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$/).withMessage("Solo admite letras"),
+    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$/).withMessage("Solo admite letras"),
 
     check("tipo")
     .notEmpty().withMessage("El campo type es Obligatorio")
     .isString().withMessage("El campo type debe ser de tipo String")
-    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$/).withMessage("Solo admite letras"),
+    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$/).withMessage("Solo admite letras"),
 
     check("descripcion")
     .notEmpty().withMessage("El campo description es Obligatorio")
     .isString().withMessage("El campo description debe ser de tipo String")
-    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$/).withMessage("Solo admite letras"),
+    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$/).withMessage("Solo admite letras"),
 
     check("zonas")
     .notEmpty().withMessage("El campo zone es Obligatorio")
     .isString().withMessage("El campo zone debe ser de tipo String")
-    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$/).withMessage("Solo admite letras"),
+    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$/).withMessage("Solo admite letras"),
 
     check("precios")
     .notEmpty().withMessage("El campo prices es Obligatorio")
@@ -30,12 +30,12 @@ export const CheckPostAllPases= [
     check("horario")
     .notEmpty().withMessage("El campo schedule es Obligatorio")
     .isNumeric().withMessage("El campo schedule debe ser de tipo Int")
-    .matches(/^(0?[1-9]|1[0-2]):[0-5]\\d\\s?(am|pm|AM|PM)$/).withMessage("Solo admite numeros"),
+    .matches(/^.*$/).withMessage("Solo admite numeros"),
 
     check("dias")
     .notEmpty().withMessage("El campo days_opening es Obligatorio")
     .isString().withMessage("El campo days_opening debe ser de tipo String")
-    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$/).withMessage("Solo admite letras"),
+    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$/).withMessage("Solo admite letras"),
 ]
 
 
