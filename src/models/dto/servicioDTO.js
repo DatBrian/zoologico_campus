cdee482469438dbf3f93f14c2e9acf40b6ec7bc4
JoseCabrejoVillarCampus <cabre920903@gmail.com>
2023-08-31 13:51:09 -0500

    refactor: :recycle: Arreglos las colecciones, agragadas colecciones
    Arreglamos las colecciones con el tipo date, ademas agregamos dos colecciones nuevas, que mejoran el entendimiento de la aplicacion con su DTOs

--- src/models/dto/servicioDTO.js
+++ src/models/dto/servicioDTO.js
@@ -12,14 +12,11 @@ export const CheckPostAllServicios= [
     .isString().withMessage("El campo description debe ser de tipo String")
     .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]+$/).withMessage("Solo admite letras"),
 
-<<<<<<< HEAD
-=======
     check("amount")
     .notEmpty().withMessage("El campo amount es Obligatorio")
     .isNumeric().withMessage("El campo amount debe ser de tipo Int")
     .matches(/^[0-9,.#@\\s-]+$/).withMessage("Solo admite numeros"),
 
->>>>>>> b69bb4b (refactor: :recycle: Correcion de los Alias En los DTOs)
     check("price")
     .notEmpty().withMessage("El campo price es Obligatorio")
     .isNumeric().withMessage("El campo price debe ser de tipo Int")
