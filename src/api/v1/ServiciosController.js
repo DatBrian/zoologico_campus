efea8689bc50ca63652862425f37a84cff9176be
JoseCabrejoVillarCampus <cabre920903@gmail.com>
2023-09-03 10:28:05 -0500

    refactor: :recycle: Instanciacion de los servicioas, cambiada a nula
    Cambiamos la instanciacion del constructor a nula, para evitar problemas en las promesass, la llmamos directamente a la funcion

--- src/api/v1/ServiciosController.js
+++ src/api/v1/ServiciosController.js
@@ -3,11 +3,12 @@ import ClientError from "../../utils/ClientError.js";
 
 class ServiciosController{
     constructor(){
-        this.service = new ServiciosService();
+        this.service = null;
     }
 
     async getAll(_req, res){
         try {
+            this.service = new ServiciosService();
             const Servicios = await this.service.getAll();
             res.json(Servicios);
         } catch (error) {
@@ -17,6 +18,7 @@ class ServiciosController{
     }
     async getById(req, res){
         try {
+            this.service = new ServiciosService();
             const animal = await this.service.getById(req.query.id);
             res.json(animal);
         } catch (error) {
@@ -26,6 +28,7 @@ class ServiciosController{
     }
     async insertOne (req, res){
         try {
+            this.service = new ServiciosService();
             const response = await this.service.insertOne(req.body);
             res,json(response)
         } catch (error) {
@@ -35,6 +38,7 @@ class ServiciosController{
     }
     async updateOne (req, res){
         try {
+            this.service = new ServiciosService();
             const body = await req.body;
             const id = await req.query.id;
             const response = await this.service.updateOne(id, body);
@@ -46,6 +50,7 @@ class ServiciosController{
     }
     async deleteOne (req, res){
         try {
+            this.service = new ServiciosService();
             const response = await this.service.deleteOne(req.query.id);
             res.json(response);
         } catch (error) {
