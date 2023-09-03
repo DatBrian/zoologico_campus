08caf2335583e21827e82aa973aa1b3373015af4
JoseCabrejoVillarCampus <cabre920903@gmail.com>
2023-09-03 10:28:05 -0500

    refactor: :recycle: Instanciacion de los servicioas, cambiada a nula
    Cambiamos la instanciacion del constructor a nula, para evitar problemas en las promesass, la llmamos directamente a la funcion

--- src/api/v1/ServiciosController.js
+++ src/api/v1/ServiciosController.js
@@ -8,7 +8,6 @@ class ServiciosController{
 
     async getAll(_req, res){
         try {
-            console.log("asdasa");
             this.service = new ServiciosService();
             const Servicios = await this.service.getAll();
             res.json(Servicios);
