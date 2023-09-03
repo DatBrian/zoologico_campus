c921f4148b1bc970f81be429fce75e60121fc270
JoseCabrejoVillarCampus <cabre920903@gmail.com>
2023-09-03 10:29:07 -0500

    refactor: :recycle: Cambios en los esquemas
    Cambiamos los esquemas para mejorar la validacion de los dto

--- src/models/schemas/index.js
+++ src/models/schemas/index.js
@@ -4,6 +4,8 @@ import EventosSchema from "./EventosSchema.js";
 import PasesSchema from "./PasesSchema.js";
 
 import ServiciosSchema from "./ServiciosSchema.js";
+import JornadasSchema from "./JornadasSchema.js";
+import RestauranteCafeteriaSchema from "./RestauranteCafeteriasSchema.js";
 
 const schemas = [
     AnimalesSchema,
@@ -11,6 +13,9 @@ const schemas = [
     PasesSchema,
     ServiciosSchema,
     EmpleadosSchema,
+    JornadasSchema,
+    RestauranteCafeteriaSchema
+
 ];
 
 export { schemas };
