9ffc69f31763425acf021becb65b228f2d9b5e6b
JoseCabrejoVillarCampus <cabre920903@gmail.com>
2023-09-03 01:17:58 -0500

    feat: :sparkles: Creamos los archivos de Servicio
    para las colecciones faltantes creamos los archivos de servicio

---
+++ src/services/EventosService.js
@@ -0,0 +1,54 @@
+import { ObjectId } from "mongodb";
+import EventosRepository from "../repositories/EventosRepository.js";
+import ClientError from "../utils/ClientError.js";
+
+class EventosService   {
+    constructor(){
+        this.repository = new EventosRepository();
+    }
+
+    async getAll(){
+        try {
+            return await this.repository.getAll();
+        } catch (error) {
+            new ClientError(400, "Error al obtener los Eventos Servicio");
+            throw error.message;
+        }
+    }
+    async getById(id){
+        try {
+            const objectId = new ObjectId;
+            return await this.repository.getByid(objectId);
+        } catch (error) {
+            new ClientError(400, "Error al obtener los Eventos Servicio");
+            throw error.message;
+        }
+    }
+    async insertOne(body){
+        try {
+            return await this.repository.insertOne(body);
+        } catch (error) {
+            new ClientError(400, "Error al obtener los Eventos Servicio");
+            throw error.message;
+        }
+    }
+    async updateOne(id,body){
+        try {
+            const ObjectId =  new ObjectId(id)
+            return await this.repository.updateOne(ObjectId, body);
+        } catch (error) {
+            new ClientError(400, "Error al obtener los Eventos Servicio");
+            throw error.message;
+        }
+    }
+    async deleteOne(id){
+        try {
+            const ObjectId =  new ObjectId(id)
+            return await this.repository.updateOne(ObjectId);
+        } catch (error) {
+            new ClientError(400, "Error al obtener los Eventos Servicio");
+            throw error.message;
+        }
+    }
+}
+export default EventosService;
