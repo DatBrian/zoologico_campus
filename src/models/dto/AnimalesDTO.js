c8db2ac20e9301c4e8eb63439c6819132c943ae6
JoseCabrejoVillarCampus <cabre920903@gmail.com>
2023-09-04 13:16:54 -0500

    refactor: :recycle: Correccion de los esquemas

--- src/models/dto/AnimalesDTO.js
+++ src/models/dto/AnimalesDTO.js
@@ -4,41 +4,33 @@ export const AnimalesDTO= [
 
     check("name")
     .notEmpty().withMessage("El campo name es Obligatorio")
-    .isString().withMessage("El campo name debe ser de tipo String")
-    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$/).withMessage("Solo admite letras"),
+    .isString().withMessage("El campo name debe ser de tipo String"),
 
     check("species")
-    .notEmpty().withMessage("El campo species es Obligatorio")
-    .isString().withMessage("El campo species debe ser de tipo String"),
+    .notEmpty().withMessage("El campo species es Obligatorio"),
 
     check("class")
     .notEmpty().withMessage("El campo class es Obligatorio")
-    .isString().withMessage("El campo class debe ser de tipo String")
-    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$/).withMessage("Solo admite letras"),
+    .isString().withMessage("El campo class debe ser de tipo String"),
 
     check("sub_class")
     .notEmpty().withMessage("El campo sub_class es Obligatorio")
-    .isString().withMessage("El campo sub_class debe ser de tipo String")
-    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$/).withMessage("Solo admite letras"),
+    .isString().withMessage("El campo sub_class debe ser de tipo String"),
 
     check("origin")
     .notEmpty().withMessage("El campo origin es Obligatorio")
-    .isString().withMessage("El campo origin debe ser de tipo String")
-    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$/).withMessage("Solo admite letras"),
+    .isString().withMessage("El campo origin debe ser de tipo String"),
 
     check("state")
     .notEmpty().withMessage("El campo state es Obligatorio")
-    .isString().withMessage("El campo state debe ser de tipo String")
-    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$/).withMessage("Solo admite letras"),
+    .isString().withMessage("El campo state debe ser de tipo String"),
 
     check("curiosity")
-    .notEmpty().withMessage("El campo curiosity es Obligatorio")
-    .isString().withMessage("El campo curiosity debe ser de tipo String"),
+    .notEmpty().withMessage("El campo curiosity es Obligatorio"),
 
     check("zone")
     .notEmpty().withMessage("El campo zone es Obligatorio")
-    .isString().withMessage("El campo zone debe ser de tipo String")
-    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$/).withMessage("Solo admite letras"),
+    .isString().withMessage("El campo zone debe ser de tipo String"),
 
     check("belonging_area")
     .notEmpty().withMessage("El campo belonging_area es Obligatorio")
--- src/models/schemas/AnimalesSchema.js
+++ src/models/schemas/AnimalesSchema.js
@@ -7,64 +7,55 @@ class AnimalesSchema {
         this.collection = this.database.collection(this.entity);
     }
 
-    static properties(){
+    static properties() {
         return {
             _id: {
-                bsonType: "objectId",
+                bsonType: 'objectId',
             },
             nombre: {
                 bsonType: "string",
                 pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
-                description:
-                    "Debe informar el campo name y este debe ser un string",
+                description: "Debe informar el campo name y este debe ser un string"
             },
             especie: {
                 bsonType: "string",
                 pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
-                description:
-                    "Debe informar el campo species y este debe ser un string",
+                description: "Debe informar el campo species y este debe ser un string"
             },
             clase: {
                 bsonType: "string",
                 pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
-                description:
-                    "Debe informar el campo class y este debe ser un string",
+                description: "Debe informar el campo class y este debe ser un string"
             },
             sub_clase: {
                 bsonType: "string",
                 pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
-                description:
-                    "Debe informar el campo sub_class y este debe ser un string",
+                description: "Debe informar el campo sub_class y este debe ser un string"
             },
             pais_origen: {
                 bsonType: "string",
                 pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
-                description:
-                    "Debe informar el campo origin y este debe ser un string",
+                description: "Debe informar el campo origin y este debe ser un string"
             },
             estado: {
                 bsonType: "string",
                 pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
-                description:
-                    "Debe informar el campo state y este debe ser un string",
+                description: "Debe informar el campo state y este debe ser un string"
             },
             dato_curioso: {
                 bsonType: "string",
                 pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
-                description:
-                    "Debe informar el campo curiosity y este debe ser un string",
+                description: "Debe informar el campo curiosity y este debe ser un string"
             },
             zona: {
                 bsonType: "string",
                 pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
-                description:
-                    "Debe informar el campo zone y este debe ser un string",
+                description: "Debe informar el campo zone y este debe ser un string"
             },
             area: {
                 bsonType: "string",
                 pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
-                description:
-                    "Debe informar el campo belonging_area y este debe ser un string",
+                description: "Debe informar el campo belonging_area y este debe ser un string"
             },
         }
     }
@@ -73,84 +64,65 @@ class AnimalesSchema {
         try {
             await this.database.createCollection(this.entity, {
                 capped: true,
-                size: 10000,
-                max: 10,
-                validator: {
-                    $jsonSchema: {
-                        bsonType: "object",
-                        additionalProperties: false,
-                        required: [
-                            "nombre",
-                            "especie",
-                            "clase",
-                            "sub_clase",
-                            "pais_origen",
-                            "estado",
-                            "dato_curioso",
-                            "zona",
-                            "area",
-                        ],
-                        properties: {
-                            _id: {
-                                bsonType: "objectId",
-                            },
-                            nombre: {
-                                bsonType: "string",
-                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
-                                description:
-                                    "Debe informar el campo name y este debe ser un string",
-                            },
-                            especie: {
-                                bsonType: "string",
-                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
-                                description:
-                                    "Debe informar el campo species y este debe ser un string",
-                            },
-                            clase: {
-                                bsonType: "string",
-                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
-                                description:
-                                    "Debe informar el campo class y este debe ser un string",
-                            },
-                            sub_clase: {
-                                bsonType: "string",
-                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
-                                description:
-                                    "Debe informar el campo sub_class y este debe ser un string",
-                            },
-                            pais_origen: {
-                                bsonType: "string",
-                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
-                                description:
-                                    "Debe informar el campo origin y este debe ser un string",
-                            },
-                            estado: {
-                                bsonType: "string",
-                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
-                                description:
-                                    "Debe informar el campo state y este debe ser un string",
-                            },
-                            dato_curioso: {
-                                bsonType: "string",
-                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
-                                description:
-                                    "Debe informar el campo curiosity y este debe ser un string",
-                            },
-                            zona: {
-                                bsonType: "string",
-                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
-                                description:
-                                    "Debe informar el campo zone y este debe ser un string",
-                            },
-                            area: {
-                                bsonType: "string",
-                                pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
-                                description:
-                                    "Debe informar el campo belonging_area y este debe ser un string",
-                            },
-                        },
-                    },
+    size: 14000,
+    max: 10,
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            additionalProperties: false,
+            required: ["nombre", "especie", "clase", "sub_clase","pais_origen", "estado", "dato_curioso", "zona", "area"],
+            properties: {
+                _id: {
+                    bsonType: 'objectId',
                 },
+                nombre: {
+                    bsonType: "string",
+                    pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
+                    description: "Debe informar el campo name y este debe ser un string"
+                },
+                especie: {
+                    bsonType: "string",
+                    pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
+                    description: "Debe informar el campo species y este debe ser un string"
+                },
+                clase: {
+                    bsonType: "string",
+                    pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
+                    description: "Debe informar el campo class y este debe ser un string"
+                },
+                sub_clase: {
+                    bsonType: "string",
+                    pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
+                    description: "Debe informar el campo sub_class y este debe ser un string"
+                },
+                pais_origen: {
+                    bsonType: "string",
+                    pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
+                    description: "Debe informar el campo origin y este debe ser un string"
+                },
+                estado: {
+                    bsonType: "string",
+                    pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
+                    description: "Debe informar el campo state y este debe ser un string"
+                },
+                dato_curioso: {
+                    bsonType: "string",
+                    pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
+                    description: "Debe informar el campo curiosity y este debe ser un string"
+                },
+                zona: {
+                    bsonType: "string",
+                    pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
+                    description: "Debe informar el campo zone y este debe ser un string"
+                },
+                area: {
+                    bsonType: "string",
+                    pattern: "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ,.#@\\s-]*$",
+                    description: "Debe informar el campo belonging_area y este debe ser un string"
+                },
+            }
+        }
+    }
             });
         } catch (error) {
             new ClientError(`Error al generar la colección de ${this.entity}`);
@@ -160,67 +132,116 @@ class AnimalesSchema {
 
     async createData() {
         try {
-            await this.collection.insertMany([
-                {
-                    nombre: "León",
-                    especie: "Panthera leo",
-                    clase: "Mammalia",
-                    sub_clase: "Theria",
-                    pais_origen: "África",
-                    estado: "Vulnerable",
-                    dato_curioso:
-                        "Los leones son conocidos por ser los reyes de la selva.",
-                    zona: "Savana",
-                    area: "Reserva Natural",
-                },
-                {
-                    nombre: "Elefante africano",
-                    especie: "Loxodonta africana",
-                    clase: "Mammalia",
-                    sub_clase: "Theria",
-                    pais_origen: "África",
-                    estado: "Amenazado",
-                    dato_curioso:
-                        "Los elefantes son los animales terrestres más grandes del mundo.",
-                    zona: "Selva",
-                    area: "Parque Nacional",
-                },
-                {
-                    nombre: "Tigre de Bengala",
-                    especie: "Panthera tigris tigris",
-                    clase: "Mammalia",
-                    sub_clase: "Theria",
-                    pais_origen: "India",
-                    estado: "En peligro",
-                    dato_curioso:
-                        "Los tigres son cazadores solitarios y excelentes nadadores.",
-                    zona: "Bosque",
-                    area: "Reserva de Tigres",
-                },
-                {
-                    nombre: "Pingüino emperador",
-                    especie: "Aptenodytes forsteri",
-                    clase: "Aves",
-                    subclase: "Neornithes",
-                    pais_origen: "Antártida",
-                    estado: "Casi amenazado",
-                    dato_curioso:
-                        "Los pingüinos emperador son conocidos por su impresionante forma de crianza.",
-                    zona: "Antártida",
-                    area: "Colonia de Pingüinos",
-                },
-                {
-                    nombre: "Tortuga marina",
-                    especie: "Chelonia mydas",
-                    clase: "Reptilia",
-                    sub_clase: "Diapsida",
-                    pais_origen: "Océanos del mundo",
-                    estado: "En peligro crítico",
-                    dato_curioso:
-                        "Las tortugas marinas viajan miles de millas para anidar en las playas donde nacieron.",
-                    zona: "Océano",
-                    area: "Hábitat Marino",
-                },
+            await this.collection.insertMany([{
+                nombre: "León",
+                especie: "Panthera leo",
+                clase: "Mamífero",
+                sub_clase: "Terrestre",
+                pais_origen: "África",
+                estado: "Vulnerable",
+                dato_curioso: "Los leones viven en grupos llamados manadas.",
+                zona: "Savana",
+                area: "África"
+            },
+            {
+                nombre: "Elefante",
+                especie: "Loxodonta africana",
+                clase: "Mamífero",
+                sub_clase: "Terrestre",
+                pais_origen: "África",
+                estado: "En peligro",
+                dato_curioso: "Los elefantes tienen una increíble memoria.",
+                zona: "Selva",
+                area: "África"
+            },
+            {
+                nombre: "Pingüino Emperador",
+                especie: "Aptenodytes forsteri",
+                clase: "Aves",
+                sub_clase: "Aves marinas",
+                pais_origen: "Antártida",
+                estado: "Casi amenazado",
+                dato_curioso: "Los pingüinos emperador son famosos por su crianza en condiciones extremas.",
+                zona: "Antártida",
+                area: "Aviario Polar"
+            },
+            {
+                nombre: "Tigre de Bengala",
+                especie: "Panthera tigris tigris",
+                clase: "Mamífero",
+                sub_clase: "Terrestre",
+                pais_origen: "Asia",
+                estado: "En peligro de extinción",
+                dato_curioso: "Los tigres son excelentes nadadores y disfrutan del agua.",
+                zona: "Bosque tropical",
+                area: "Asia"
+            },
+            {
+                nombre: "Cebra",
+                especie: "Equus zebra",
+                clase: "Mamífero",
+                sub_clase: "Terrestre",
+                pais_origen: "África",
+                estado: "Preocupación menor",
+                dato_curioso: "Las cebras tienen patrones únicos de rayas que las identifican individualmente.",
+                zona: "Savana",
+                area: "África"
+            },
+            {
+                nombre: "Delfín Nariz de Botella",
+                especie: "Tursiops truncatus",
+                clase: "Mamífero",
+                sub_clase: "Acuático",
+                pais_origen: "Océanos del mundo",
+                estado: "Datos insuficientes",
+                dato_curioso: "Los delfines usan ecolocalización para comunicarse y cazar.",
+                zona: "Océano",
+                area: "Mares y océanos"
+            },
+            {
+                nombre: "Oso Polar",
+                especie: "Ursus maritimus",
+                clase: "Mamífero",
+                sub_clase: "Terrestre",
+                pais_origen: "Ártico",
+                estado: "Vulnerable",
+                dato_curioso: "Los osos polares son excelentes nadadores y cazadores marinos.",
+                zona: "Hielo polar",
+                area: "Ártico"
+            },
+            {
+                nombre: "Hipopótamo",
+                especie: "Hippopotamus amphibius",
+                clase: "Mamífero",
+                sub_clase: "Acuático",
+                pais_origen: "África",
+                estado: "Vulnerable",
+                dato_curioso: "A pesar de su aspecto tranquilo, los hipopótamos son muy agresivos y peligrosos.",
+                zona: "Río",
+                area: "África"
+            },
+            {
+                nombre: "Gorila",
+                especie: "Gorilla gorilla",
+                clase: "Mamífero",
+                sub_clase: "Terrestre",
+                pais_origen: "África",
+                estado: "En peligro",
+                dato_curioso: "Los gorilas viven en grupos liderados por un macho dominante llamado silverback.",
+                zona: "Selva",
+                area: "África"
+            },
+            {
+                nombre: "Panda Gigante",
+                especie: "Ailuropoda melanoleuca",
+                clase: "Mamífero",
+                sub_clase: "Terrestre",
+                pais_origen: "China",
+                estado: "Vulnerable",
+                dato_curioso: "Los pandas gigantes se alimentan principalmente de bambú.",
+                zona: "Bosque de bambú",
+                area: "Asia"
+            }
             ]);
         } catch (error) {
             new ClientError(
